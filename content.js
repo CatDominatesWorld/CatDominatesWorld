20fabdb56378ad3ea09a49520d12cbcd6c04a8ae
Youngseok Choe <dev.youngseok@gmail.com>
2020-07-26 16:30:01 +0900

    Post request to backend

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
-console.log(document.body.outerHTML);
+var body = document.body.outerHTML;
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+    }
+};
+
+xhr.open("POST", API_URL, true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(JSON.stringify({
+    content: body,
+    level: 4
+}));
