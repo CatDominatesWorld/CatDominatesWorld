55746c849785e884de2631c05fbc3dbe85c2c502
Youngseok Choe <yschoelife@gmail.com>
2020-07-27 16:53:47 +0900

    Use background.js to send level

--- content.js
+++ content.js
@@ -1,15 +1,23 @@
-var body = document.body.outerHTML;
-var xhr = new XMLHttpRequest();
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.message === 'TabUpdated') {
+        API_URL = "https://192.249.19.244:1343/translate"
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
+        var body = document.body.innerHTML;
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+            console.log("response");
+            if (this.readyState == 4 && this.status == 200) {
+                var nyanContent = this.responseText
+                document.getElementsByTagName('body')[0].innerHTML = nyanContent
+            }
+        };
+
+        xhr.open("POST", API_URL, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+            content: body,
+            level: 4
+        }));
     }
-};
-
-xhr.open("POST", API_URL, true);
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send(JSON.stringify({
-    content: body,
-    level: 4
-}));
+  })
