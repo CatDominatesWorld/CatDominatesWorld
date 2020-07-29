0b4076a21a33f5151bb534337e60190cfac22775
Youngseok Choe <yschoelife@gmail.com>
2020-07-29 13:04:41 +0900

    Fix not working bug

--- background.js
+++ background.js
@@ -1,15 +1,13 @@
 var level = 0;
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-      if (changeInfo.status === 'complete' && level !== 0 ) {
-        chrome.tabs.sendMessage(tabId, {
-          message: 'TabUpdated',
-          level: level
-        });
-      }
-    })
-  });
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete' && level !== 0 ) {
+      chrome.tabs.sendMessage(tabId, {
+        message: 'TabUpdated',
+        level: level
+      });
+    }
+  })
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
